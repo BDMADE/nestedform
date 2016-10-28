983587462cb6283a285deca95d5cac43ba407e1c
hmtanbir <tanbir2025@gmail.com>
2016-10-28 19:25:13 +0600

    hidden field remove

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -70,6 +70,6 @@ class CustomersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def customer_params
-      params.require(:customer).permit(:name, orders_attributes: [:id, :item,:customer_id])
+      params.require(:customer).permit(:name, orders_attributes: [:id, :item])
     end
 end
