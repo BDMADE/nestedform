dfbf47d1f17485bff1ff0aa090911cc5f14aac0d
hmtanbir <tanbir2025@gmail.com>
2016-10-28 16:20:37 +0600

    order params add

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -15,6 +15,7 @@ class CustomersController < ApplicationController
   # GET /customers/new
   def new
     @customer = Customer.new
+    3.times { @customer.orders.build }
   end
 
   # GET /customers/1/edit
@@ -69,6 +70,6 @@ class CustomersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def customer_params
-      params.require(:customer).permit(:name)
+      params.require(:customer).permit(:name,orders_attributes: [:id, :item])
     end
 end
